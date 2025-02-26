8594ade408f45a34f3955fe0704dfd6438f2e3fd
Marius Ahsmus <marius.ahsmus@gmail.com>
2025-02-26 17:37:56 +0100

    feat: layout-page-structure, headbar, sidebar

---
+++ src/data.ts
@@ -0,0 +1,14 @@
+export const projects = [
+    {
+        title: "Luna Labs UI",
+        description: "A collection of accessible and customizable React components.",
+    },
+    {
+        title: "Luna Labs Design",
+        description: "A collection of accessible and customizable Figma components.",
+    },
+    {
+        title: "Luna Labs Icons",
+        description: "A collection of accessible and customizable SVG icons.",
+    }
+]

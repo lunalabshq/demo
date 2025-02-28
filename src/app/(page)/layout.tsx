afa246dd55b756dc51f483318731e5cc89272f7e
Marius Ahsmus <marius.ahsmus@gmail.com>
2025-02-28 20:07:19 +0100

    feat: new sidebar implementation

--- src/app/(page)/layout.tsx
+++ src/app/(page)/layout.tsx
@@ -2,20 +2,12 @@
 
 import type {ReactNode} from "react"
 import CustomSidebar from "@/components/CustomSidebar"
-import HeadBar from "@/components/HeadBar"
-import {SidebarProvider} from "lunalabs-ui"
 
 export default function Layout({children}: Readonly<{ children: ReactNode }>) {
     return (
-        <SidebarProvider>
-            <div className={"w-screen h-screen flex flex-row"}>
-                <CustomSidebar/>
-                <div className={"h-screen w-full flex flex-col"}>
-                    <HeadBar/>
-                    {children}
-                </div>
-
-            </div>
-        </SidebarProvider>
+        <div className={"w-screen h-screen flex flex-row"}>
+            <CustomSidebar/>
+            {children}
+        </div>
     )
 }
--- src/data.ts
+++ src/data.ts
@@ -2,13 +2,16 @@ export const projects = [
     {
         title: "Luna Labs UI",
         description: "A collection of accessible and customizable React components.",
+        url: "https://luna-labs.github.io/icons/",
     },
     {
         title: "Luna Labs Design",
         description: "A collection of accessible and customizable Figma components.",
+        url: "https://luna-labs.github.io/icons/",
     },
     {
         title: "Luna Labs Icons",
         description: "A collection of accessible and customizable SVG icons.",
+        url: "https://luna-labs.github.io/icons/",
     }
 ]
